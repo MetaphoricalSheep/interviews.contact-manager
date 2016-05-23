3e21746baa9d7deca96209a3da44a7a6fff9476c
Pieter-Uys Fourie <pieteruys.fourie@gmail.com>
2016-05-23 16:07:08 +0200

    Title href now points to hp

--- views/header.php
+++ views/header.php
@@ -1,7 +1,7 @@
 <header>
     <!--Navbar-->
     <nav class="navbar navbar-fixed-top navbar-dark bg-primary">
-        <a class="navbar-brand" href="#">Contacts</a>
+        <a class="navbar-brand" href="/">Contacts</a>
         <!--Search form-->
         <form class="form-inline search-form">
             <input class="form-control" id="search-form-keyword" type="text" placeholder="Search">
